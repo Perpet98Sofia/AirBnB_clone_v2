c42e6077f67661e140c5b5144b56401187dd3c2f
Perpet98Sofia <perpetualakwiri@gmail.com>
2024-02-28 16:23:13 +0000

    add-correct

--- console.py
+++ console.py
@@ -115,7 +115,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-         try:
+        try:
             if not args:
                 raise SyntaxError()
             arg_list = args.split(" ")
