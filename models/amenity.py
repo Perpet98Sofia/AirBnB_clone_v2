49bb851ec2ea98bdc5a1dde6d8cfe8e52282cbc4
Perpet98Sofia <134222072+Perpet98Sofia@users.noreply.github.com>
2024-03-02 17:28:39 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ instances amenities """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from models.city import City
+from models.user import User
+from models.place import place_amenity
 from sqlalchemy.orm import relationship
-from models.place import Place
-from os import getenv
-
-STORAGE = getenv("HBNB_TYPE_STORAGE")
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from os import environSTORAGE = getenv("HBNB_TYPE_STORAGE")
 
 
 class Amenity(BaseModel, Base):
@@ -15,7 +15,7 @@ class Amenity(BaseModel, Base):
     if STORAGE == "db":
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
-            'Place', secondary=Place.place_amenity)
+            'Place', secondary=place_amenity, back_populates="amenities")
 
     else:
         name = ""
