284266519c92f4f7ae228d5be2a6c9d55debbc85
Perpet98Sofia <134222072+Perpet98Sofia@users.noreply.github.com>
2024-03-02 17:39:15 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.place import Place
+from models.user import User
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey
+from os import environ
 
 
 class Review(BaseModel, Base):
@@ -12,11 +16,14 @@ class Review(BaseModel, Base):
         user_id: user id
         text: review description
     """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-
-    """place_id =
-    user_id =
-    text = """
+   
+ if (storage_engine == 'db'):
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey("places.id"))
+        user_id = Column(String(60), ForeignKey("users.id"))
+        text = Column(String(1024), nullable=False)
+        place = relationship("Place", back_populates="reviews")
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
