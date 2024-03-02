935bef4d462a435ebc283161427ac831c85bae92
Perpet98Sofia <134222072+Perpet98Sofia@users.noreply.github.com>
2024-03-02 17:25:06 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,8 @@
 """create a unique FileStorage instance for your application
 """
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -11,12 +13,22 @@ from models.place import Place
 from models.review import Review
 
 
-from os import getenv
+from os import environ
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
+dummy_classes = {"BaseModel": BaseModel, "User": User,
+                 "Review": Review, "City": City,
+                 "State": State, "Place": Place,
+                 "Amenity": Amenity}
+
+dummy_tables = {"states": State, "cities": City,
+                "users": User, "places": Place,
+                "reviews": Review, "amenities": Amenity}
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
+if (storage_engine == "db"):
     storage = DBStorage()
+    storage.reload()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()
+    storage.reload()
